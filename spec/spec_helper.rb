32fbd05f9dbe2b0615138243afddb33e0470a5cd
Hermann Mayer <hermann.mayer92@gmail.com>
2019-07-15 12:18:59 +0200

    Added a monkey patch for Ruby 2.6/Rails 4.2 test issues.
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,18 @@ require 'factory_bot_instrumentation'
 require 'timecop'
 require 'pp'
 
+# See: https://github.com/rails/rails/issues/34790#issuecomment-450502805
+if RUBY_VERSION >= '2.6.0' && Rails.version < '5'
+  class ActionController::TestResponse < ActionDispatch::TestResponse
+    def recycle!
+      # hack to avoid MonitorMixin double-initialize error:
+      @mon_mutex_owner_object_id = nil
+      @mon_mutex = nil
+      initialize
+    end
+  end
+end
+
 # Setup a default timezone for the tests
 Time.zone = 'Europe/Berlin'
 
