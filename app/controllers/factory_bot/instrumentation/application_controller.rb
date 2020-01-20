801fcf9459ce6307901d019d753a5523db7c8100
Hermann Mayer <hermann.mayer92@gmail.com>
2020-01-20 17:00:53 +0100

    Added support for custom before action logics. (#7)
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- app/controllers/factory_bot/instrumentation/application_controller.rb
+++ app/controllers/factory_bot/instrumentation/application_controller.rb
@@ -3,5 +3,33 @@
 module FactoryBot::Instrumentation
   # A base engine application controller.
   class ApplicationController < ActionController::API
+    # Extend our core functionality to support easy authentication logics
+    include ActionController::HttpAuthentication::Basic::ControllerMethods
+    include ActionController::HttpAuthentication::Digest::ControllerMethods
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+
+    # Allow the users to implement additional instrumentation-wide before
+    # action logic, like authentication
+    before_action do |controller|
+      if FactoryBot::Instrumentation.configuration.before_action
+        instance_eval &FactoryBot::Instrumentation.configuration.before_action
+      end
+    end
+
+    protected
+
+    # A simple shortcut for Basic Auth on Rails 4.2+. Just configure the
+    # username and password and you're ready to check the current request.
+    #
+    # @param username [String] the required user name
+    # @param password [String] the required password of the user
+    # @param realm [String] the authentication realm
+    def basic_auth(username:, password:, realm: 'Instrumentation')
+      authenticate_or_request_with_http_basic(realm) \
+        do |given_name, given_password|
+        ActiveSupport::SecurityUtils.secure_compare(given_name, username) &
+          ActiveSupport::SecurityUtils.secure_compare(given_password, password)
+      end
+    end
   end
 end
